e21edaac8419e4ba839c2b9be3d855802e2a613f
santosMichele <santos.michele@ifpb.edu.br>
2019-03-23 11:13:56 -0300

    polinomio

--- Roteiro1/polinomioTerceiroGrau.py
+++ Roteiro1/polinomioTerceiroGrau.py
@@ -4,3 +4,11 @@
 ################################## IFPB - Eng. de Computação ##################################
 ###############################################################################################
 
+coeficienteA = int(input())
+coeficienteB = int(input())
+coeficienteC = int(input())
+coeficienteD = int(input())
+valorDeX = int(input())
+
+polinomio = coeficienteA*(valorDeX**3) + coeficienteB*(valorDeX**2) + coeficienteC*(valorDeX) + coeficienteD
+print(polinomio)
