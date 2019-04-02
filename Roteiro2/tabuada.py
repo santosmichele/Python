c1595456e385397d1f29a74e7f886471fb837d54
santosMichele <santos.michele@ifpb.edu.br>
2019-04-01 23:59:48 -0300

    tabuada

--- Roteiro2/tabuada.py
+++ Roteiro2/tabuada.py
@@ -6,13 +6,31 @@
 
 
 
+cont = 1    
+inicio =  int(input())
+while inicio <= 0 or inicio > 9:
+    print ("Insira um número inicial entre 1 e 9")
+    inicio =  int(input())
 
+fim =  int(input())    
+while fim <= 0 or fim > 9:    
+    print ("Insira um número final entre 1 e 9")
+    fim =  int(input())
 
-tabuada = 1
-número = 1
-while tabuada <= 10:
-     print("%d x %d = %d" % (tabuada, número, tabuada * número))
-     número += 1
-     if número == 11:
-         número = 1
-         tabuada += 1
+    
+if inicio > fim:
+    print ("SEM RESPOSTA")
+                  
+
+while inicio <= fim:
+
+    
+    print("%d x %d = %d" % (inicio, cont, inicio * cont))
+    cont += 1
+    
+    if cont == 10:
+         cont = 1
+         inicio += 1 
+         print ()
+         
+    
