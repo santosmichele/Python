7108dba75a895d98b39752bb2a678b53367d0d05
santosMichele <santos.michele@ifpb.edu.br>
2019-04-01 23:16:30 -0300

    vaiParaOnde

--- Roteiro2/vaiParaOnde.py
+++ Roteiro2/vaiParaOnde.py
@@ -4,3 +4,22 @@
 ################################## IFPB - Eng. de Computação ##################################
 ###############################################################################################
 
+
+
+
+destino = 'SEM DESTINO'
+distanciaDestino = 0
+while True:
+    cidade = input()
+    cidade = cidade.upper()
+    if (cidade) == 'FIM':
+        break
+
+    distancia = int(input())
+    valor  = int(input())
+    valor *=2
+
+    if distanciaDestino < distancia and valor <= 300:
+        distanciaDestino = distancia
+        destino = cidade
+print (destino)
