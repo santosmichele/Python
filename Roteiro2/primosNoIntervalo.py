70b9d8af5b9b6fe6243f39577b173f38d3ad9b6a
santosMichele <santos.michele@ifpb.edu.br>
2019-04-02 08:30:48 -0300

    primosNoIntervalo

--- Roteiro2/primosNoIntervalo.py
+++ Roteiro2/primosNoIntervalo.py
@@ -4,3 +4,25 @@
 ################################## IFPB - Eng. de Computação ##################################
 ###############################################################################################
 
+
+inicioIntervalo = int(input())
+fimIntervalo = int(input())
+
+if (inicioIntervalo > fimIntervalo):
+    menor = fimIntervalo
+    fimIntervalo =inicioIntervalo
+    inicioIntervalo = menor
+
+for i in range(inicioIntervalo, fimIntervalo+1):
+
+    divisores = 0
+    ehPrimo = True
+
+    for n in range(1, i):
+        if (i % n == 0):
+            divisores += 1
+        if (divisores > 1):
+            ehPrimo = False
+    if (ehPrimo):
+        if (i != 1):
+            print(i)
