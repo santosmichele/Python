01cbe2b29cc12a2b8faa94c489b850b49745ef91
santosMichele <santos.michele@ifpb.edu.br>
2019-04-03 11:04:49 -0300

    pokemon

---
+++ Roteiro4/pokemon.py
@@ -0,0 +1,11 @@
+###############################################################################################
+###################################### Michele Santos #########################################
+################################# Algoritimos e Programação I #################################
+################################## IFPB - Eng. de Computação ##################################
+###############################################################################################
+
+#utilizado um dicionario, mas poderia ser uma lista e buscar pelo indice por exemplo, porém é importante aprender os tipos existentes e como/quando utilizá-los 
+pokemons = {'1': 'Bulbassauro', '2': 'Charmander', '3': 'Squirtle' }
+pokemonEscolhido = input()
+
+print (pokemons[pokemonEscolhido] )
