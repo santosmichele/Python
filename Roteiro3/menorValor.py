33935e8a9237dda444c1610c6fa84886695bb254
santosMichele <santos.michele@ifpb.edu.br>
2019-04-03 08:58:27 -0300

    menorValor

--- Roteiro3/menorValor.py
+++ Roteiro3/menorValor.py
@@ -7,5 +7,7 @@
 numeroElementos = int(input())
 listaElementos = input()
 listaElementos = listaElementos.split(" ", numeroElementos)
+for i in range(len(listaElementos)):
+    listaElementos[i] = int(listaElementos[i])
 
 print("Menor valor: %i\nPosicao: %i" % (int(min(listaElementos)), int(listaElementos.index(min(listaElementos)))) )
